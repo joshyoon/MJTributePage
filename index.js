69c0f2bea7ff397e754b419bb836bdfbf7b2dd68
joshyoon <jyoon716@gmail.com>
2017-02-26 19:56:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('#btnJosh').click(function() {
+        $('#emailForm').attr('action','mailto:jyoon716@gmail.com');
+    });
+});
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+  // Prevent default anchor click behavior
+  event.preventDefault();
+
+  // Store hash
+  var hash = this.hash;
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 900, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+    });
+  });
+})
